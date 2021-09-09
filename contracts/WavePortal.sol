99e3c3b3c0226d4738780fa1a92f26463cc4c95c
Duco Gaillard <ducogaillard@gmail.com>
2021-09-09 21:17:43 +0200

    finalv1

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,6 +7,7 @@ import "hardhat/console.sol";
 contract WavePortal {
 
     //event NewWave(address indexed from, uint timestamp, string message);
+    uint private seed;
 
     struct Wave {
         string message;
@@ -18,6 +19,7 @@ contract WavePortal {
 
     uint waveCounter;
     mapping(address => uint) public wavesPerPerson;
+    mapping(address => uint) public lastWavedAt;
 
     constructor() payable {
         console.log("Hello, this is a smart contract");
@@ -26,6 +28,10 @@ contract WavePortal {
 
     // wave with text
     function wave(string memory _message) public {
+        require(lastWavedAt[msg.sender] + 30 seconds < block.timestamp, "wait 15 min");
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
         waveCounter += 1;
         wavesPerPerson[msg.sender] += 1;
         console.log(msg.sender, " just waved at you for the ", wavesPerPerson[msg.sender], "th time, with the following message:");
@@ -33,10 +39,19 @@ contract WavePortal {
         waves.push(Wave(_message, msg.sender, block.timestamp));
         //emit NewWave(msg.sender, block.timestamp, _message);
 
-        uint prizeAmount = 0.0001 ether;
-        require(prizeAmount <= address(this).balance, "Contract out of money!");
-        (bool success,) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "failed to withdraw money");
+        uint randomNumber = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random nummber =",randomNumber);
+        seed = randomNumber;
+
+
+        if(randomNumber < 50) {
+            console.log(msg.sender, "Won!");
+            uint prizeAmount = 0.0001 ether;
+            require(prizeAmount <= address(this).balance, "Contract out of money!");
+            (bool success,) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "failed to withdraw money");
+        }
+
 
 
     }
