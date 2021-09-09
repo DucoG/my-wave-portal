d2fade26131588fca08b9d8dec92e6b3228b955b
Duco Gaillard <ducogaillard@gmail.com>
2021-09-10 00:33:09 +0200

    wavebucks contract v1

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,39 +5,128 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+
+    //save the waves
     struct Wave {
         string message;
-        address waver;
+        string waver;
+        string wavee;
         uint timestamp;
+        uint bucksAttached;
     }
+    //save incomming and outgoing waves
+    mapping(uint => Wave[]) incommingWaves;
+    mapping(uint => Wave[]) outgoigWaves;
 
-    Wave[] waves;
-
+    //Wave[] waves;
     uint waveCounter;
-    mapping(address => uint) public wavesPerPerson;
+
+    //save users
+    struct User {
+        string username;
+        address addr;
+        uint wavebuckBalance;
+        uint totalWaved;
+        uint lastWave;
+        uint id;
+    }
+    //save users
+    User[] users;
+
+
+    //check if user exists
+    mapping(address => uint) getIdFromAddress;
+
+    //username for displaying address book
+    string[] usernames;
+
+    //get id from username
+    mapping(string => uint) getIdFromUsername;
+
+    //return userdata
+
+    //get userdata from address
+    //mapping(address => User) users;
+
 
     constructor() {
         console.log("Hello, this is a smart contract");
         waveCounter = 0;
     }
 
-    // wave with text
-    function wave(string memory _message) public {
+    //register
+    function register(string memory _username) public{
+        require(getIdFromAddress[msg.sender] < 1, "You're already registered! You can delete your account by using the 'deleteAccount' function." );
+        //registration process
+        getIdFromAddress[msg.sender] = users.length + 1;
+        getIdFromUsername[_username] = users.length + 1;
+        users.push(User(_username, msg.sender, 10, 0, 0, users.length + 1));
+        usernames.push(_username);
+        console.log("Hi %s, you are now a registered user and have received 10 wavebucks for free!", _username);
+    }
+
+    //delete account
+
+
+    //Temporary!!!
+    //get user data
+    function getAllUserData() public view returns (User[] memory) {
+        require(getIdFromAddress[msg.sender] > 0, "You must be registered in order to read user data");
+        return users;
+    }
+
+    // wave with message and money
+    function wave(string memory _receiverUsername , string memory _message, uint _sentBucks) public {
+        require(getIdFromAddress[msg.sender] > 0, "Please register before waving in order to receive your free wavebucks :)");
+        require(getIdFromUsername[_receiverUsername] > 0, "that username isn't registered!");
+
+
+        //make variables
+        string memory senderName = users[getIdFromAddress[msg.sender] - 1].username;
+        uint senderId = getIdFromAddress[msg.sender];
+        uint receiverId = getIdFromUsername[_receiverUsername];
+
+        require(_sentBucks <= users[senderId].wavebuckBalance, "You dont have enough money!");
+
+
         waveCounter += 1;
-        wavesPerPerson[msg.sender] += 1;
-        console.log(msg.sender, " just waved at you for the ", wavesPerPerson[msg.sender], "th time, with the following message:");
-        console.log("Got message: %s", _message);
-        waves.push(Wave(_message, msg.sender, block.timestamp));
+
+        //save wave as incoming and outgoing
+        incommingWaves[receiverId].push(Wave(_message, senderName, _receiverUsername, block.timestamp, _sentBucks));
+        outgoigWaves[senderId].push(Wave(_message, senderName, _receiverUsername, block.timestamp, _sentBucks));
+        users[receiverId - 1].wavebuckBalance += _sentBucks;
+        users[senderId - 1].wavebuckBalance -= _sentBucks - 1;
+
+        
+        console.log("You just waved at %s with for with the following message:", _receiverUsername);
+        console.log(_message);
+        console.log("You attached %s WaveBucks to your wave", _sentBucks);
+        console.log("You received %s WaveBucks for your wave!", 1);
+
+
+        //waves.push(Wave(_message, msg.sender, block.timestamp, _sentBucks));
         //emit NewWave(msg.sender, block.timestamp, _message);
     }
 
-    function getAllWaves() view public returns(Wave[] memory) {
-        return waves;
+    function getIncommingWaves() public view returns(Wave[] memory){
+        uint senderId = getIdFromAddress[msg.sender];
+        return incommingWaves[senderId];
     }
+
+    function getOutgoingWaves() public view returns(Wave[] memory){
+        uint senderId = getIdFromAddress[msg.sender];
+        return outgoigWaves[senderId];
+    }
+
+    // function getAllWaves() view public returns(Wave[] memory) {
+    //     return waves;
+    // }
 
     function getWaveCounter() public view returns (uint) {
         console.log("Our wavecounter is at %d", waveCounter);
         return waveCounter;
     }
+
+
 }
 
--- scripts/run.js
+++ scripts/run.js
@@ -2,7 +2,7 @@ const { hexStripZeros } = require("ethers/lib/utils");
 async function main() {
 
     //Get owner
-    const [owner, randoPerson] = await ethers.getSigners();
+    const [owner, rand1, rand2] = await ethers.getSigners();
     
     //deploy
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
@@ -11,6 +11,48 @@ async function main() {
     console.log("Contract deployed to:", waveContract.address);
     console.log("Contract owner is:", owner.address);
 
+    let registrationTxn = await waveContract.connect(owner).register("duuxster")
+    await registrationTxn.wait()
+
+    let userdata = await waveContract.getAllUserData()
+    console.log(userdata)
+
+    registrationTxn = await waveContract.connect(rand1).register("rand1")
+    await registrationTxn.wait()
+
+    registrationTxn = await waveContract.connect(rand2).register("rand2")
+    await registrationTxn.wait()
+
+    userdata = await waveContract.getAllUserData()
+    console.log(userdata)
+
+    console.log("sending msg")
+    let waveTxn = await waveContract.wave("rand1", "This is a cool message", 3)
+    await waveTxn.wait()
+
+    userdata = await waveContract.getAllUserData()
+    console.log(userdata)
+
+    console.log("waves1")
+    let outgoing = await waveContract.getOutgoingWaves()
+    console.log(outgoing)
+    let incomming = await waveContract.getIncommingWaves()
+    console.log(incomming)
+
+    console.log("waves2")
+    outgoing = await waveContract.connect(rand1).getOutgoingWaves()
+    console.log(outgoing)
+    incomming = await waveContract.connect(rand1).getIncommingWaves()
+    console.log(incomming)
+
+    console.log("waves3")
+
+    outgoing = await waveContract.connect(rand2).getOutgoingWaves()
+    console.log(outgoing)
+    incomming = await waveContract.connect(rand2).getIncommingWaves()
+    console.log(incomming)
+
+    /*
     //testing
     let allWaves = await waveContract.getAllWaves()
     console.log("current stored waves", allWaves)
@@ -23,7 +65,7 @@ async function main() {
 
     allWaves = await waveContract.getAllWaves()
     console.log("current stored waves", allWaves)
-
+    */
 }
 
 main()
